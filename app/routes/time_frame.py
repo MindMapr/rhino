c19fee9a9faf48ae50dea5788c5e1878e602fb47
Cthaar <cthaar20@student.aau.dk>
2025-03-27 14:44:47 +0100

    updated timeframe for protected routes

--- app/routes/time_frame.py
+++ app/routes/time_frame.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, Depends
+from typing import Annotated
 from pydantic import BaseModel
 from datetime import datetime, date, time
 
@@ -6,6 +7,7 @@ from ..models.time_frame import TimeFrame, WorkTimeIntervals
 from ..models.user import User
 from ..database.mongodb import database
 from ..controllers.time_frame import TimeFrameList
+from ..utils.auth import get_current_user
 
 # Setup collection
 collection = database.time_frame
@@ -16,6 +18,9 @@ router = APIRouter(prefix="/time_frame", tags=["time_frame"])
 # Controllers
 list_routes = TimeFrameList(collection)
 
+# Dependencies
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
 class CreateTimeFrame(BaseModel):
     start_date: date
     end_date: date
@@ -23,10 +28,10 @@ class CreateTimeFrame(BaseModel):
     include_weekend: bool
 
 @router.post("", status_code=201)
-async def create_time_frame(params: CreateTimeFrame, user: User):
+async def create_time_frame(params: CreateTimeFrame, current_user: user_dependency):
     # Converting the request to an instance of time_frame. Using this approach instead of model_dump to ensure user_id is included
     time_frame = TimeFrame(
-        user = user.user_id,
+        user_id = current_user["_id"],
         start_date=params.start_date,
         end_date=params.end_date,
         work_time_frame_intervals=params.work_intervals,
